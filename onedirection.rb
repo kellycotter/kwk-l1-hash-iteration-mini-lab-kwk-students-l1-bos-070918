e59f57da80cfd85b237eb1308aa0d9ba5b6c95fe
Kelly Cotter <kjc853@gmail.com>
2018-07-12 14:05:56 +0000

    automatically backed up by learn

---
+++ onedirection.rb
@@ -0,0 +1,15 @@
+one_direction = {"Zayn" => 25,
+                 "Liam" => 24,
+                 "Louis" => 26,
+                 "Harry" => 24,
+                 "Niall" => 24 
+}
+
+# How can you print out all of the members and their ages like the following:
+# Zayn is 25 years old.
+# Liam is 24 years old.
+# etc...
+
+one_direction.each do |onedirection|
+  puts "#{onedirection}"
+end
