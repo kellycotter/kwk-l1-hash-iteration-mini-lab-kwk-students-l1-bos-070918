11737ba2237e7291124d3faad1cc554e85fa26b8
Kelly Cotter <kjc853@gmail.com>
2018-07-12 13:25:17 +0000

    automatically backed up by learn

--- capital.rb
+++ capital.rb
@@ -1,1 +1,29 @@
-states = {"Massachusetts" => "MA"
+# You have 2 hashes, one which maps state names to state abbreviations, and one which maps state abbreviations to their capital:
+
+# states = {"Massachusetts" => "MA",
+#           "Wisconsin" => "WI",
+#           "New Jersey" => "NJ",
+#           "Colorado" => "CO"}
+
+# capitals = {"MA" => "Boston",
+#             "WI" => "Madison",
+#             "NJ" => "Trenton",
+#             "CO" => "Denver"}
+
+#     Level 1: Write some code which given a state name ("Massachusetts") outputs its capital ("Boston")
+#     Level 2: Handle the case when a state's information is not known by returning "Unknown"
+#     Level 3: Now let's go the other way. Given a capital name ("Denver"), return the state name for which it is the capital ("Colorado")
+
+states = {"Massachusetts" => "MA",
+          "Wisconsin" => "WI",
+          "New Jersey" => "NJ",
+          "Colorado" => "CO"}
+
+capitals = {"MA" => "Boston",
+            "WI" => "Madison",
+			"NJ" => "Trenton",
+	 		"CO" => "Denver"}
+	 		
+	 		
+	 		
+capitals("MA")
