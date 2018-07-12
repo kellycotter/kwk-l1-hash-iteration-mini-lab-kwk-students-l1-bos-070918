29a711b880ce356adc698182e9beaa6fd83d29d3
Kelly Cotter <kjc853@gmail.com>
2018-07-12 13:47:54 +0000

    automatically backed up by learn

--- capital.rb
+++ capital.rb
@@ -1,33 +1,33 @@
-# You have 2 hashes, one which maps state names to state abbreviations, and one which maps state abbreviations to their capital:
+# # You have 2 hashes, one which maps state names to state abbreviations, and one which maps state abbreviations to their capital:
+
+# # states = {"Massachusetts" => "MA",
+# #           "Wisconsin" => "WI",
+# #           "New Jersey" => "NJ",
+# #           "Colorado" => "CO"}
+
+# # capitals = {"MA" => "Boston",
+# #             "WI" => "Madison",
+# #             "NJ" => "Trenton",
+# #             "CO" => "Denver"}
+
+# #     Level 1: Write some code which given a state name ("Massachusetts") outputs its capital ("Boston")
+# #     Level 2: Handle the case when a state's information is not known by returning "Unknown"
+# #     Level 3: Now let's go the other way. Given a capital name ("Denver"), return the state name for which it is the capital ("Colorado")
 
 # states = {"Massachusetts" => "MA",
 #           "Wisconsin" => "WI",
 #           "New Jersey" => "NJ",
-#           "Colorado" => "CO"}
+#           "Colorado" => "CO",
+#           "New York" => "NY"
+# }
 
 # capitals = {"MA" => "Boston",
 #             "WI" => "Madison",
-#             "NJ" => "Trenton",
-#             "CO" => "Denver"}
-
-#     Level 1: Write some code which given a state name ("Massachusetts") outputs its capital ("Boston")
-#     Level 2: Handle the case when a state's information is not known by returning "Unknown"
-#     Level 3: Now let's go the other way. Given a capital name ("Denver"), return the state name for which it is the capital ("Colorado")
-
-states = {"Massachusetts" => "MA",
-          "Wisconsin" => "WI",
-          "New Jersey" => "NJ",
-          "Colorado" => "CO",
-          "New York" => "NY"
-}
-
-capitals = {"MA" => "Boston",
-            "WI" => "Madison",
-			"NJ" => "Trenton",
-	 		"CO" => "Denver",
-      "NY" => "ALbany"
-}
-puts "What state do you want to know the capital of?"	 		
+# 			"NJ" => "Trenton",
+# 	 		"CO" => "Denver",
+#       "NY" => "ALbany"
+# }
+# puts "What state do you want to know the capital of?"	 		
 
 def given 
 puts capitals[states[given]]
